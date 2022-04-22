ed34d84990f77258f8decc5d54d0298224488933
Andrew Vaughan <liljazzman@Andrews-MacBook-Pro.local>
2022-04-21 21:37:00 -0700

    made updates and bug fixes

--- src/Medications.jsx
+++ src/Medications.jsx
@@ -7,27 +7,32 @@ const Medications = () => {
   const [q, setQ] = useState("")
   const [meds, setMeds] = useState([])
 
-
+//  || med.strength.includes(query)
   useEffect(() => {
     setMeds(JsonData)
   }, [])
 
+  //currently set to SF for facility
+
   const filterFunction = (query) => {
-    console.log('running')
-    query.length>0?
-    setMeds(
-      JsonData.filter((med) => {
-        return (
-          med.name.toLowerCase().includes(q.toLowerCase())
-        )
-      }).filter((med) => (med.facility === "SF"))
-    ):setMeds(JsonData)
+    console.log('running', typeof query)
+    const filtered = JsonData.filter((med) => {
+      return (
+        ((med.name.toString().toLowerCase().indexOf(query.toString().toLowerCase())) >= 0) ||
+        ((med.strength.indexOf(query)) >= 0)
+      )
+
+    }).filter((med) => (med.facility === "SF"))
+    if (q) {console.log(q)
+      setMeds(filtered)}
+    else {setMeds(JsonData)}
+    console.log('still running', typeof query)
   }
 
   const handleSearchChange = (event) => {
     event.preventDefault()
     setQ(event.target.value);
-    filterFunction(event.target.value)
+    filterFunction(q)
   }
   return (
 
@@ -37,14 +42,16 @@ const Medications = () => {
   type="text"
   value={q}
   placeholder="Search"
-  onChange={handleSearchChange}/>
+  onChange={(event) => handleSearchChange(event)}/>
+  <select>
   {meds.map((med) => {
     return (
-      <ul>
-        {med.name}: {med.facility}
-      </ul>
+        <option>
+          {med.name}: {med.facility}-- {med.strength}
+        </option>
     )
   })}
+   </select>
   </>
   )
 }
